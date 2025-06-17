87b2eaae56cf3918cfbc1243ea478c7b3caac81d
ericksgmes <erick.dicksan@gmail.com>
2025-06-17 14:01:23 -0300

    refactor: put composite code in Composite instead of Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,23 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        FileRemover fileRemover = new FileRemover();
-
-        Directory apoo = new Directory("apoo");
-        File file1 = new File("Refactoring.md", "this is the content of the file");
-        File file2 = new File("Como aumentar o antebraço.json", "é só fazer exercicios blablabla");
-        File file3 = new File("Design Patterns.md", "o padrão de projeto compositeblablabla");
-        File file4 = new File("Clash of clans base atualizada.txt", "isso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txtisso nao deveria ser um txt");
-
-        apoo.add(file1, file2, file3);
-
-        apoo.printSize();
-        apoo.printStructure();
-        file1.printContents();
-        file4.printSize();
-
-        fileRemover.remove(file4);
-
-        fileRemover.remove(apoo);
+        System.out.println("Hello World");
     }
 }
